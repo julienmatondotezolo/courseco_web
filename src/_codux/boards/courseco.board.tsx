3333179f217261aba9d8f05d7c424e6f3c700cdb
julienmatondotezolo <julien.matondo-tezolo@student.ehb.be>
2023-04-24 22:15:44 +0200

    codux configuration

---
+++ src/_codux/boards/courseco.board.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createBoard } from "@wixc3/react-board";
+
+export default createBoard({
+  name: "courseco",
+  Board: () => (
+    <div>
+      <nav>
+        <a href="/home">Home</a> | <a href="/projects">Projects</a> | <a href="/about">About</a> |{" "}
+        <a href="/contact">Contact Us</a>
+      </nav>
+    </div>
+  ),
+});
