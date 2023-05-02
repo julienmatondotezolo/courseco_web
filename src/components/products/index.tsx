e7f7e64dbb31283f3e6d468150f6f089166e13e0
julienmatondotezolo <julien.matondo-tezolo@student.ehb.be>
2023-05-02 18:32:30 +0200

    styling productsCard

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -9,22 +9,32 @@ type Props = {
 
 export function Products({ products }: Props) {
   return (
-    <div className="grid sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-x-2 md:gap-x-5 gap-y-10 overflow-hidden">
+    <div className="grid sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-x-2 md:gap-x-5 gap-y-10 overflow-hidden">
       {products.map((product) => (
-        <div className="relative flex flex-wrap rounded-lg w-[100%] border " key={product.id}>
+        <div className="relative flex flex-wrap rounded-xl w-[100%] border" key={product.id}>
           {product.promotion ? (
-            <p className="absolute px-3 py-2 text-xs text-white bg-red-notification">{product.promotion}</p>
+            <p className="absolute rounded-tl-xl rounded-r-lg px-3 py-2 text-xs text-white bg-red-notification">
+              {product.promotion}
+            </p>
           ) : (
             ""
           )}
-          <article className="flex flex-col p-5">
+          <button className="absolute bottom-0 right-0 primaryButton rounded-bl-none rounded-tr-none">+</button>
+
+          <section className="flex flex-col flex-1 justify-between p-5">
             <figure className="flex justify-center w-[100%] p-5 object-contain">
               <Image src={product.images} alt={product.name} width={100} height={100} />
             </figure>
-            <p>{product.store}</p>
-            <h3>{product.name}</h3>
-            {product.discountedPriceFormatted ? <p>{product.discountedPriceFormatted}</p> : <p>{product.price}</p>}
-          </article>
+            <article>
+              <p className="text-xs">{product.store}</p>
+              <h3 className="text-base font-medium">{product.name}</h3>
+            </article>
+            {product.discountedPriceFormatted ? (
+              <p className="text-xl font-bold">{product.discountedPriceFormatted}</p>
+            ) : (
+              <p className="text-xl font-bold">{product.price}</p>
+            )}
+          </section>
         </div>
       ))}
     </div>
