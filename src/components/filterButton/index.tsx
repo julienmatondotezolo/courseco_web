af521855e188db165cf6dd8212e330fc884aff6b
julienmatondotezolo <julien.matondo-tezolo@student.ehb.be>
2023-05-03 19:40:35 +0200

    adding filters & filterButton

---
+++ src/components/filterButton/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function FilterButton() {
+  return (
+    <button className="filterButton mb-4">
+      <input className="float-left w-[20px] h-[20px] mr-2" type="checkbox" />
+      <p className="float-left">
+        Magasins Carrefour <span className="text-gray-400">(184)</span>
+      </p>
+    </button>
+  );
+}
